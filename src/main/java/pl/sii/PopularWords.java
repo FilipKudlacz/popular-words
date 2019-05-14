a2cf8aeaa598eacb973415a8481bdc0838294780
FilipKudlacz <kudlacz1998@gmail.com>
2019-05-14 20:34:04 +0200

    Revert "Revert "Whole aplication working, test not quite passed""
    This reverts commit b4ff7fee97908071e2a0e73df5fc6d03be001219.

--- src/main/java/pl/sii/PopularWords.java
+++ src/main/java/pl/sii/PopularWords.java
@@ -1,8 +1,8 @@
 package pl.sii;
 
-import org.apache.commons.lang3.NotImplementedException;
+import java.io.*;
+import java.util.*;
 
-import java.util.Map;
 
 public class PopularWords {
 
@@ -12,7 +12,63 @@ public class PopularWords {
         result.entrySet().forEach(System.out::println);
     }
 
-    public Map<String, Long> findOneThousandMostPopularWords() {
-        throw new NotImplementedException("TODO implementation");
+    public Map<String, Long> findOneThousandMostPopularWords(){
+        Map<String, Long> wordFrequency = convertFileToFrequencyMap();
+        wordFrequency = sortMapDescendingByValueAndLimitToThousand(wordFrequency);
+
+        return wordFrequency;
     }
+
+    private Map<String, Long> convertFileToFrequencyMap(){
+        File input = new File(Objects.requireNonNull(getClass().getClassLoader().getResource("3esl.txt")).getFile());
+        Map<String, Long> wordFrequency = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(input))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] wordsInLine = line.split(" ");
+
+                for (String word : wordsInLine) {
+                    if(!wordFrequency.containsKey(word)){
+                        Long value = 1L;
+                        wordFrequency.put(word, value);
+                    }else{
+                        Long value = wordFrequency.get(word) + 1;
+                        wordFrequency.put(word, value);
+                    }
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return wordFrequency;
+    }
+
+    private Map<String,Long> sortMapDescendingByValueAndLimitToThousand(Map<String,Long> map){
+        List<Map.Entry<String, Long> > list =
+                new LinkedList<>(map.entrySet());
+
+        Collections.sort(list, new Comparator<>() {
+            public int compare(Map.Entry<String, Long> o1,
+                               Map.Entry<String, Long> o2) {
+                return (o2.getValue()).compareTo(o1.getValue());
+            }
+        });
+
+        int i = 0;
+        HashMap<String, Long> temp = new LinkedHashMap<>();
+        for (Map.Entry<String, Long> aa : list) {
+            if( i == 1000){
+                break;
+            }
+            temp.put(aa.getKey(), aa.getValue());
+            i++;
+        }
+
+
+        return temp;
+    }
+
 }
